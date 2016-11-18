23b657103cc6a204421613140cdb01ede8f4b83d
alwinruby <alwinsolanky@gmail.com>
2016-11-18 13:12:12 +0000

    First few changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ //$("#main").append("Alwin Solanky");
+
+var awesomeThoughts = "I am Alwin Solanky and I'm AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+console.log(funThoughts);
+console.log(awesomeThoughts);
+$("#main").append(funThoughts);
